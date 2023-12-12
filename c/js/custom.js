b81154bba13a5f038aad7d31a8f4c8700d31623c
huaiche <cksh11224@gmail.com>
2023-12-12 13:57:35 -0600

    1st ver.

--- c/js/custom.js
+++
@@ -1,8 +0,0 @@
-function getCurrentYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-    console.log(currentYear);
-}
-
-getCurrentYear();
